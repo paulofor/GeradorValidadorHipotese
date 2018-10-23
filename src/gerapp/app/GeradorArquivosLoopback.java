b3ca15105ce89311817fcb0eccb0cb3b051a0101
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-10-23 17:09:25 -0300

    tratando assinc

--- src/gerapp/app/GeradorArquivosLoopback.java
+++ src/gerapp/app/GeradorArquivosLoopback.java
@@ -1,28 +1,18 @@
 package gerapp.app;
 
 import gerapp.modelo.Aplicacao;
-import gerapp.modelo.AtributoEntidade;
-import gerapp.modelo.Entidade;
-import gerapp.modelo.FiltroColecao;
-import gerapp.modelo.ProcValor;
-import gerapp.modelo.RegraColecao;
-import gerapp.modelo.RelacionamentoEntidade;
-import gerapp.regracolecao.AplicacaoRegraColecao;
-import gerapp.regracolecao.EntidadeRegraColecao;
-import gerapp.regracolecao.FabricaRegra;
-import gerapp.regracolecao.filtro.EntidadeFiltro;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import jet.wrappers.base.ClasseWrapper;
-import loopback.cliente.repositorio.AplicacaoRepositorio;
+import loopback.android.callback.ListCallback;
+import loopback.cliente.modelo.EntidadeRest;
+import loopback.cliente.repositorio.EntidadeRepositorio;
 import loopback.remoting.adapters.RestAdapter;
 import br.com.digicom.lib.dao.DaoException;
 
 public abstract class GeradorArquivosLoopback extends GeradorArquivosBase{
 	
+	private List listaCorrente = null;
 	
 	public void setAplicacao(Aplicacao aplicacao)  throws DaoException{
 		this.aplicacao = new AplicacaoWrapper(aplicacao);
@@ -30,15 +20,29 @@ public abstract class GeradorArquivosLoopback extends GeradorArquivosBase{
 	
 	public List getListaEntidade() throws DaoException{
 		RestAdapter adapter = new RestAdapter("http://validacao.kinghost.net:21101/api");
-		AplicacaoRepositorio rep = adapter.createRepository(AplicacaoRepositorio.class);
-		
-		
-		EntidadeRegraColecao entidadeRC = FabricaRegra.getInstancia().getEntidadeRegraColecao();
-		List listaSaida = null;
-		EntidadeFiltro filtro = entidadeRC.getFiltro();
-		filtro.setCodigoAplicacao(aplicacao.getId());
-		listaSaida = entidadeRC.ListaCorrente();
-		return listaSaida;
+		EntidadeRepositorio rep = adapter.createRepository(EntidadeRepositorio.class);
+
+		rep.findByIdAplicacao(aplicacao.getId(), new ListCallback<EntidadeRest>() { 
+         
+			@Override
+			public void onError(Throwable t) {
+				// TODO Auto-generated method stub
+				t.printStackTrace();
+			}
+
+			@Override
+			public void onSuccess(List<EntidadeRest> lista) {
+				// TODO Auto-generated method stub
+				for (EntidadeRest item : lista) {
+					System.out.println("ID:" + item.getId());
+				}
+				listaCorrente = lista;
+			} 
+        });
+		do {
+			
+		} while (listaCorrente == null);
+		return listaCorrente;
 	}
 	
 	
