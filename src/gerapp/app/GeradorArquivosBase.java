41e7b31e9679b4c2ef8d3b149481f42d364821b3
Paulo <paulofore@gmail.com>
2018-11-08 22:43:37 -0200

    encode

--- src/gerapp/app/GeradorArquivosBase.java
+++ src/gerapp/app/GeradorArquivosBase.java
@@ -23,10 +23,14 @@ import gerapp.regracolecao.filtro.RegraColecaoFiltro;
 import gerapp.regracolecao.filtro.RelacionamentoEntidadeFiltro;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -38,77 +42,80 @@ public abstract class GeradorArquivosBase {
 
 	protected AplicacaoWrapper aplicacao = null;
 	protected List<ClasseWrapper> listaClasse = null;
-	
-	
 
-	
-	public void setAplicacao(Aplicacao aplicacao)  throws DaoException{
+	public void setAplicacao(Aplicacao aplicacao) throws DaoException {
 
-		AplicacaoRegraColecao aplicacaoRC = FabricaRegra.getInstancia().getAplicacaoRegraColecao();
+		AplicacaoRegraColecao aplicacaoRC = FabricaRegra.getInstancia()
+				.getAplicacaoRegraColecao();
 		aplicacao = aplicacaoRC.obtemPorChave(aplicacao.getIdAplicacao());
 		this.aplicacao = new AplicacaoWrapper(aplicacao);
 	}
-	
-	protected void geraArquivoFonte(String texto, String nomeArquivo) throws IOException {
-		FileWriter x = new FileWriter(nomeArquivo); 
-		x.write(texto); 
-		x.close();
+
+	protected void geraArquivoFonte(String texto, String nomeArquivo)
+			throws IOException {
+		FileOutputStream fos = new FileOutputStream(nomeArquivo);
+		Writer w = new BufferedWriter(new OutputStreamWriter(fos, "US-ASCII"));
+		w.write(texto);
+		w.flush();
+		w.close();
 	}
 
-	protected void copiaArquivo(String origem, String destino) throws IOException {
+	protected void copiaArquivo(String origem, String destino)
+			throws IOException {
 		String texto = leArquivoTexto(origem);
 		geraArquivoFonte(texto, destino);
 	}
-	
-	public String leArquivoTexto(String fileName) throws IOException{
-	    String returnValue = "";
-	    FileReader file;
-	    String line = "";
-	    file = new FileReader(fileName);
-	    BufferedReader reader = new BufferedReader(file);
-	    try {
-	    	while ((line = reader.readLine()) != null) {
-	    		returnValue += line + "\n";
-	    	} 
-	    }finally {
-	    	reader.close();
-	    }
-	    return returnValue;
+
+	public String leArquivoTexto(String fileName) throws IOException {
+		String returnValue = "";
+		FileReader file;
+		String line = "";
+		file = new FileReader(fileName);
+		BufferedReader reader = new BufferedReader(file);
+		try {
+			while ((line = reader.readLine()) != null) {
+				returnValue += line + "\n";
+			}
+		} finally {
+			reader.close();
+		}
+		return returnValue;
 	}
-	
-	
+
 	protected void limpaCaminho(String caminhoArquivo) throws IOException {
 		File caminho = new File(caminhoArquivo);
-		if (caminho.isDirectory()) {  
-			for (File arquivo : caminho.listFiles()) { 
+		if (caminho.isDirectory()) {
+			for (File arquivo : caminho.listFiles()) {
 				if (arquivo.isFile())
-					arquivo.delete();  
+					arquivo.delete();
 			}
 		}
 	}
 
-	
 	protected void criaCaminhoSeNaoExiste(String caminho) throws IOException {
 		if (!existe(caminho)) {
 			criaCaminho(caminho);
 		}
 	}
+
 	protected void criaCaminho(String caminhoArquivo) throws IOException {
 		File caminho = new File(caminhoArquivo);
 		caminho.mkdirs();
 	}
+
 	protected boolean existe(String nomeCompleto) throws IOException {
 		File arquivo = new File(nomeCompleto);
 		return arquivo.exists();
 	}
-	
+
 	protected abstract ClasseWrapper criaWrapper(Entidade entidade);
+
 	protected Configuracao criaConfiguracao() {
 		Configuracao confW = new Configuracao(aplicacao);
 		return confW;
 	}
-	
-	private void montaListaClasse() throws DaoException{
+
+	private void montaListaClasse() throws DaoException {
 		System.out.println("Inicio Monta Lista Classe");
 		listaClasse = new ArrayList<ClasseWrapper>();
 		List lista = getListaEntidade();
@@ -117,27 +124,36 @@ public abstract class GeradorArquivosBase {
 			Entidade entidade = (Entidade) iterador.next();
 			System.out.println("Vai tratar " + entidade.getNome());
 			ClasseWrapper corrente = criaWrapper(entidade);
-			List<AtributoEntidade> listaAtributos = getListaAtributos(corrente.getId());
-			
-			AtributoEntidade attChave = this.getAtributo(corrente.getIdChave(),listaAtributos);
+			List<AtributoEntidade> listaAtributos = getListaAtributos(corrente
+					.getId());
+
+			AtributoEntidade attChave = this.getAtributo(corrente.getIdChave(),
+					listaAtributos);
 			corrente.setChave(attChave);
-			AtributoEntidade attIdent = this.getAtributo(corrente.getIdIdentificador(),listaAtributos);
+			AtributoEntidade attIdent = this.getAtributo(corrente
+					.getIdIdentificador(), listaAtributos);
 			corrente.setIdentificador(attIdent);
-			Iterator<AtributoEntidade> itAtributoEntidade = listaAtributos.iterator();
+			Iterator<AtributoEntidade> itAtributoEntidade = listaAtributos
+					.iterator();
 			while (itAtributoEntidade.hasNext()) {
 				AtributoEntidade atributo = itAtributoEntidade.next();
-				if (attChave!=null && attChave.getIdAtributoEntidade()==atributo.getIdAtributoEntidade()) {
+				if (attChave != null
+						&& attChave.getIdAtributoEntidade() == atributo
+								.getIdAtributoEntidade()) {
 					atributo.setChave(true);
 				} else {
 					atributo.setChave(false);
 				}
-				if (attIdent!=null && attIdent.getIdAtributoEntidade()==atributo.getIdAtributoEntidade()) {
+				if (attIdent != null
+						&& attIdent.getIdAtributoEntidade() == atributo
+								.getIdAtributoEntidade()) {
 					atributo.setIdentificador(true);
 				} else {
 					atributo.setIdentificador(false);
 				}
 			}
-			List<RelacionamentoEntidade> listaRelacionamento = getListaRelacionamento(corrente.getId());
+			List<RelacionamentoEntidade> listaRelacionamento = getListaRelacionamento(corrente
+					.getId());
 			List<FiltroColecao> listaFiltro = getListaFiltro(corrente.getId());
 			List<RegraColecao> listaRegra = getListaRegra(corrente.getId());
 			List<ProcValor> listaProcValor = getListaProcValor(corrente.getId());
@@ -151,17 +167,15 @@ public abstract class GeradorArquivosBase {
 		}
 		System.out.println("Final Monta Lista Classe");
 	}
-	
-	
+
 	protected void verificaDiretorios(Recursos recurso) throws IOException {
-		
+
 	}
-	
-	
+
 	public void criaArquivos() {
 		// TODO Auto-generated method stub
 		try {
-			
+
 			montaListaClasse();
 			AplicacaoWrapper.setEntidades(listaClasse);
 			Recursos recurso = new Recursos();
@@ -182,23 +196,27 @@ public abstract class GeradorArquivosBase {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public abstract void limpaArquivos(Recursos recurso) throws IOException;
-	
+
 	public abstract void criaArquivoUnico(Recursos recurso) throws IOException;
-		
-	public abstract void criaArquivoEntidade(Recursos recurso) throws IOException;
-		
+
+	public abstract void criaArquivoEntidade(Recursos recurso)
+			throws IOException;
+
 	public List getListaAtributos(long codigoEntidade) throws DaoException {
-		AtributoEntidadeRegraColecao atributoRC = FabricaRegra.getInstancia().getAtributoEntidadeRegraColecao();
+		AtributoEntidadeRegraColecao atributoRC = FabricaRegra.getInstancia()
+				.getAtributoEntidadeRegraColecao();
 		List listaSaida = null;
 		AtributoEntidadeFiltro filtro = atributoRC.getFiltro();
 		filtro.setCodigoEntidade(codigoEntidade);
 		listaSaida = atributoRC.ListaCorrente();
 		return listaSaida;
 	}
+
 	public List getListaRelacionamento(long codigo) throws DaoException {
-		RelacionamentoEntidadeRegraColecao relacRC = FabricaRegra.getInstancia().getRelacionamentoEntidadeRegraColecao();
+		RelacionamentoEntidadeRegraColecao relacRC = FabricaRegra
+				.getInstancia().getRelacionamentoEntidadeRegraColecao();
 		List listaSaida = null;
 		RelacionamentoEntidadeFiltro filtro = relacRC.getFiltro();
 		filtro.setCodigoEntidade1(codigo);
@@ -208,19 +226,22 @@ public abstract class GeradorArquivosBase {
 		listaSaida.addAll(relacRC.ListaCorrente());
 		return listaSaida;
 	}
-	
+
 	public AtributoEntidade getAtributo(long codigo) throws DaoException {
-		if (codigo==0) return null;
-		AtributoEntidadeRegraColecao atributoRC = FabricaRegra.getInstancia().getAtributoEntidadeRegraColecao();
+		if (codigo == 0)
+			return null;
+		AtributoEntidadeRegraColecao atributoRC = FabricaRegra.getInstancia()
+				.getAtributoEntidadeRegraColecao();
 		AtributoEntidade saida = atributoRC.obtemPorChave(codigo);
 		return saida;
-		
+
 	}
-	
-	public AtributoEntidade getAtributo(long codigo, List<AtributoEntidade> listaAtributo) {
+
+	public AtributoEntidade getAtributo(long codigo,
+			List<AtributoEntidade> listaAtributo) {
 		AtributoEntidade saida = null;
 		for (AtributoEntidade item : listaAtributo) {
-			if (item.getIdAtributoEntidade()==codigo) {
+			if (item.getIdAtributoEntidade() == codigo) {
 				saida = item;
 				break;
 			}
@@ -228,35 +249,39 @@ public abstract class GeradorArquivosBase {
 		return saida;
 	}
 
-	
-	public List getListaEntidade() throws DaoException{
-		EntidadeRegraColecao entidadeRC = FabricaRegra.getInstancia().getEntidadeRegraColecao();
+	public List getListaEntidade() throws DaoException {
+		EntidadeRegraColecao entidadeRC = FabricaRegra.getInstancia()
+				.getEntidadeRegraColecao();
 		List listaSaida = null;
 		EntidadeFiltro filtro = entidadeRC.getFiltro();
 		filtro.setCodigoAplicacao(aplicacao.getId());
 		listaSaida = entidadeRC.ListaCorrente();
 		return listaSaida;
 	}
-	public List getListaFiltro(long codigoEntidade) throws DaoException{
-		FiltroColecaoRegraColecao filtroRC = FabricaRegra.getInstancia().getFiltroColecaoRegraColecao();
+
+	public List getListaFiltro(long codigoEntidade) throws DaoException {
+		FiltroColecaoRegraColecao filtroRC = FabricaRegra.getInstancia()
+				.getFiltroColecaoRegraColecao();
 		List listaSaida = null;
 		FiltroColecaoFiltro filtro = filtroRC.getFiltro();
 		filtro.setCodigoEntidade(codigoEntidade);
 		listaSaida = filtroRC.ListaCorrente();
 		return listaSaida;
 	}
-	
-	public List getListaRegra(long codigoEntidade) throws DaoException{
-		RegraColecaoRegraColecao regraRC = FabricaRegra.getInstancia().getRegraColecaoRegraColecao();
+
+	public List getListaRegra(long codigoEntidade) throws DaoException {
+		RegraColecaoRegraColecao regraRC = FabricaRegra.getInstancia()
+				.getRegraColecaoRegraColecao();
 		List listaSaida = null;
 		RegraColecaoFiltro filtro = regraRC.getFiltro();
 		filtro.setCodigoEntidade(codigoEntidade);
 		listaSaida = regraRC.ListaCorrente();
 		return listaSaida;
 	}
-	
-	public List getListaProcValor(long codigoEntidade) throws DaoException{
-		ProcValorRegraColecao valorRC = FabricaRegra.getInstancia().getProcValorRegraColecao();
+
+	public List getListaProcValor(long codigoEntidade) throws DaoException {
+		ProcValorRegraColecao valorRC = FabricaRegra.getInstancia()
+				.getProcValorRegraColecao();
 		List listaSaida = null;
 		ProcValorFiltro filtro = valorRC.getFiltro();
 		filtro.setCodigoEntidadeRelacionada(codigoEntidade);
