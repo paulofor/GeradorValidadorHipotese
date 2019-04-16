a99bdb01a11e49ab8bf252dbb718e6cd4c78d03b
Paulo <paulofore@gmail.com>
2019-04-15 22:28:09 -0300

    evolucao

--- src/jet/ionic3/page/listaItem/ListaItemPageTs.java
+++ src/jet/ionic3/page/listaItem/ListaItemPageTs.java
@@ -21,18 +21,15 @@ public class ListaItemPageTs
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "";
-  protected final String TEXT_2 = NL + NL + NL + "import { Component } from '@angular/core';" + NL + "import { IonicPage, ModalController, NavController } from 'ionic-angular';" + NL + "import { Screenshot } from '@ionic-native/screenshot';" + NL + "import { ";
+  protected final String TEXT_2 = NL + "import { Component } from '@angular/core';" + NL + "import { IonicPage, NavController } from 'ionic-angular';" + NL + "import { Page } from 'ionic-angular/navigation/nav-util';" + NL + "import { ";
   protected final String TEXT_3 = "Base } from './";
-  protected final String TEXT_4 = "-base';" + NL + "" + NL + "import { ";
-  protected final String TEXT_5 = ", ";
-  protected final String TEXT_6 = "Api, LoopBackFilter } from '../../shared/sdk';" + NL + "" + NL + "@IonicPage()" + NL + "@Component({" + NL + "  selector: '";
-  protected final String TEXT_7 = "'," + NL + "  templateUrl: '";
-  protected final String TEXT_8 = ".html'" + NL + "})" + NL + "export class ";
-  protected final String TEXT_9 = " extends ";
-  protected final String TEXT_10 = "Base {" + NL + "" + NL + "\tprotected getFiltro():LoopBackFilter {" + NL + "    \treturn {};" + NL + "\t}" + NL + "" + NL + "" + NL + "" + NL + "  ionViewWillEnter() {" + NL + "    console.log('ionViewWillEnter ";
-  protected final String TEXT_11 = "');" + NL + "    this.carregaPrototipo();" + NL + "  }" + NL + "" + NL + "  ionViewDidLoad() {" + NL + "  \tconsole.log('ionViewDidLoad ";
-  protected final String TEXT_12 = "');" + NL + "  }" + NL + "  " + NL + " " + NL + "" + NL + "  " + NL + "}";
-  protected final String TEXT_13 = NL;
+  protected final String TEXT_4 = "-base';" + NL + "import { ";
+  protected final String TEXT_5 = "Api, LoopBackFilter } from '../../shared/sdk';" + NL + "" + NL + "" + NL + "@IonicPage()" + NL + "@Component({" + NL + "  selector: '";
+  protected final String TEXT_6 = "'," + NL + "  templateUrl: '";
+  protected final String TEXT_7 = ".html'" + NL + "})" + NL + "export class ";
+  protected final String TEXT_8 = " extends ";
+  protected final String TEXT_9 = "Base {" + NL + "" + NL + "  constructor(public navCtrl: NavController, protected srv: SerieTreinoApi) {" + NL + "    super(navCtrl, srv);" + NL + "  }" + NL + "" + NL + "" + NL + "  protected getPageEdicao(): Page {" + NL + "    throw new Error(\"Method not implemented.\");" + NL + "  }" + NL + "" + NL + "  protected getFiltro(): LoopBackFilter {" + NL + "    return {};" + NL + "  }" + NL + "  " + NL + "}";
+  protected final String TEXT_10 = NL;
 
   public String generate(Object argument)
   {
@@ -50,21 +47,15 @@ Configuracao conf = recursos.getConfiguracao();
     stringBuffer.append(TEXT_4);
     stringBuffer.append( tela.getEntidade().getNomeParaClasse() );
     stringBuffer.append(TEXT_5);
-    stringBuffer.append( tela.getEntidade().getNomeParaClasse() );
-    stringBuffer.append(TEXT_6);
     stringBuffer.append( tela.getNomeControle() );
-    stringBuffer.append(TEXT_7);
+    stringBuffer.append(TEXT_6);
     stringBuffer.append( tela.getArquivo() );
+    stringBuffer.append(TEXT_7);
+    stringBuffer.append( tela.getNome() );
     stringBuffer.append(TEXT_8);
     stringBuffer.append( tela.getNome() );
     stringBuffer.append(TEXT_9);
-    stringBuffer.append( tela.getNome() );
     stringBuffer.append(TEXT_10);
-    stringBuffer.append( tela.getNome() );
-    stringBuffer.append(TEXT_11);
-    stringBuffer.append( tela.getNome() );
-    stringBuffer.append(TEXT_12);
-    stringBuffer.append(TEXT_13);
     return stringBuffer.toString();
   }
 }
